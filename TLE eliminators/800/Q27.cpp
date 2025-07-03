41a80c8f20ec58f1dfd3f869e49e0bc90edf84a1
Siddhant kandi <kandisiddhant@gmail.com>
2025-07-03 13:24:26 +0530

    Solved Q27

--- TLE eliminators/800/Q27.cpp
+++ TLE eliminators/800/Q27.cpp
@@ -5,33 +5,33 @@ int main() {
     int t;
     cin>>t;
 
-    for(int i=0;i<t;i++){
+    while(t--){
         int n;
         cin>>n;
+       vector<int> arr(n,0);
+       vector<int> store;
+       
+       for(int i = 0;i<n;i++){
+        cin>>arr[i];
 
-        int size = n;
-
-        vector<int> arr(n,0);
-        vector<int> store;
-
-        for(int j = 0;j<n;j++){
-            cin>> arr[j];
-
-
-            if(arr[j] == 2){
-                store.push_back(j+1);
-            }
+        if(arr[i] == 2){
+            store.push_back(i+1);
         }
 
-        if(store.size() ==0 ){
-            cout<<1<<"\n";
-        }
-        else if(store.size() %2 == 0 ){
-            cout<<store[(store.size()/2)-1]<<"\n";
-        }
-        else{
-            cout << -1 <<"\n";
-        }
+        
+        
+    }
+
+    if(store.size() == 0){
+        cout<<1<<endl;
+    }
+    else if(store.size() % 2 !=0){
+        cout<<-1<<endl;
+    }
+    else{
+        cout<<store[store.size()/2 - 1]<<endl;
+    }
+
 
     }
 
